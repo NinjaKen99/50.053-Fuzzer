90a4e4e3d877503244f7c1f0e32efcec5c03a0ea
qazer25 <claysonsoh@gmail.com>
2024-02-22 15:16:59 +0800

    added api points for inputs

--- coap/input.py
+++ coap/input.py
@@ -5,13 +5,37 @@ import string
 
 
 
-async def send_payload(input):
-    payload = "Hello, CoAP!"
-    num_bytes = 5
-    protocol = await Context.create_client_context()
-    fuzz_bytes = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(num_bytes))
-    payload = payload[:3] + fuzz_bytes + payload[3 + num_bytes:]
-    msg = Message(code=Code.GET, uri="coap://localhost:5683/basic", payload=payload)
-    response: Message = await protocol.request(msg).response
-    print(response.payload)
+class COAPClient:
+    def __init__(self):
+        self.host = "localhost"
+        self.port = "5683"
+        self.uri = {
+            "basic": [Code.GET, Code.PUT, Code.POST, Code.DELETE], 
+            "storage": [Code.GET, Code.POST], 
+            "child": [Code.GET, Code.PUT, Code.POST, Code.DELETE],
+            "seperate": [Code.GET], 
+            "long": [Code.GET],
+            "big": [Code.GET, Code.POST],
+            "void": [],
+            "xml": [Code.GET],
+            "encoding": [Code.GET, Code.PUT, Code.POST],
+            "etag": [Code.GET, Code.PUT, Code.POST],
+            "advanced": [Code.GET, Code.PUT, Code.POST, Code.DELETE],
+            "advancedSeperate": [Code.GET, Code.PUT, Code.POST, Code.DELETE]
+        }
     
+    async def code_to_str(self, code: Code):
+        return code.__str__()
+    
+    async def get_codes(self, uri):
+        return self.uri.get(uri, None)
+    
+    async def get_uris(self):
+        return dict.keys(self.uri)
+
+    async def send_payload(self, payload, uri):
+        protocol = await Context.create_client_context()
+        msg = Message(code=Code.GET, uri=f"coap://{self.host}:{self.port}/{uri}", payload=payload)
+        response: Message = await protocol.request(msg).response
+        
+        return {response.payload, self.code_to_str(response.code)}
--- django/input.py
+++ django/input.py
@@ -3,31 +3,65 @@ import httpx
 
 
 
-async def send_payload(input):
-    client = httpx.AsyncClient(http2=True)
-    # Replace 'http://<Django app URL>' with the base URL of your Django app (e.g., 'http://localhost:8000')
-    base_url = 'http://127.0.0.1:8000'
 
-    registration_endpoint = '/accounts/register/'
 
-    registration_url = base_url + registration_endpoint
+class DajngoClient:
 
-    user_data = {
-        'username': 'john',
-        'email': 'john@example.com',
-        'password1': 'MyPassword123',
-        'password2': 'MyPassword123'  # Confirm password
-    }
+    def __init__(self):
+        self.host = "localhost"
+        self.port = 8000
+        self.client = httpx.AsyncClient(http2=True)
+        self.uri = {
+            "api/product": ["GET", "POST", "PUT", "DELETE"],
+            "login/jwt": ["POST"],
+            "accounts/register":["POST"],
+            "": ["GET"],
+            "tables": ["GET"],
+            "datab/product/add":["POST"]
+        }
 
-    try:
-        # Send a POST request to the registration endpoint with the user data
-        response =  await client.post(registration_url, json=user_data)
-        # Check if the request was successful (status code 200 or 201 for successful creation)
-        if response.status_code in (200, 201):
-            print("User registration successful!")
-            print("Response:")
-            print(response.text)
-        else:
-            print(f"User registration failed with status code: {response.status_code}")
-    except requests.exceptions.RequestException as e:
-        print("User registration request failed:", e)
+    
+    
+    async def get_codes(self, uri):
+        return self.uri.get(uri, None)
+    
+    async def get_uris(self):
+        return dict.keys(self.uri)
+    
+    async def send_payload(self, input, uri, code):
+        # Replace 'http://<Django app URL>' with the base URL of your Django app (e.g., 'http://localhost:8000')
+        base_url = f'http://{self.host}:{self.port}'
+
+        registration_endpoint = f'/{uri}'
+
+        registration_url = base_url + registration_endpoint
+
+        user_data = {
+            'username': 'john',
+            'email': 'john@example.com',
+            'password1': 'MyPassword123',
+            'password2': 'MyPassword123'  # Confirm password
+        }
+
+        try:
+            match uri:
+                
+                case "GET":
+                    pass
+                case "POST":
+                    # Send a POST request to the registration endpoint with the user data
+                    response =  await self.client.post(registration_url, json=user_data)
+                    # Check if the request was successful (status code 200 or 201 for successful creation)
+                    if response.status_code in (200, 201):
+                        print("User registration successful!")
+                        print("Response:")
+                        print(response.text)
+                    else:
+                        print(f"User registration failed with status code: {response.status_code}")
+                case "PUT":
+                    pass
+                case "DELETE":
+                    pass
+
+        except requests.exceptions.RequestException as e:
+            print("User registration request failed:", e)

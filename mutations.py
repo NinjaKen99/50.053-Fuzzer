6771b7c276fc6c568cbded687a0a3a6b52e465a4
Kendrick <kendricknghy@gmail.com>
2024-02-27 13:18:57 +0800

    Modified everything, all tested

--- mutations.py
+++ mutations.py
@@ -5,114 +5,110 @@ from random import randint
 choices = [1, 2, 4]
 bits_in_byte = 8
 
-# FUNCTIONS USED TO ASSIST MUTATION
-def to_binary_2d(input):
-    byte_count = 0
-    # Sample to split into bits
-    byte_string = ' '.join(format(ord(x), 'b') for x in input)
-    byte_set = byte_string.split(" ")
-    bit_set = []
-    for byte in byte_set:
-        byte_count += 1
-        byte_to_bit = byte.split("")
-        bit_set.append(byte_to_bit)
-    return bit_set, byte_count
 
-def to_binary_1d(input):
-    bit_count = 0
-    # Sample to split into bits
-    byte_string = ' '.join(format(ord(x), 'b') for x in input)
-    byte_set = byte_string.split(" ")
-    bit_set = []
-    for byte in byte_set:
-        byte_to_bit = byte.split("")
-        for bit in byte_to_bit:
-            bit_count += 1
-            bit_set.append(bit)
-    return bit_set, bit_count
-
-
-# HERE IS WHERE ALL FUNCTIONS TO PERFORM A MUTATION SHALL BE MADE
-
-# Flip a set number of consecutive bits
-def bitflip (input: str):
-    b, number = to_binary_1d(input)
-    # choose number of bits to change
-    count = choices[randint(0,2)]
-    start_of_change = randint(0, number - 1 - count)
-    for i in range (start_of_change, start_of_change + count):
-            if (b[i] == '1'):
-                b[i] = '0'
-            elif (b[i] == '0'):
-                b[i] = '1'
-    output = ""
-    temp = ""
-    for i in range(number):
-        temp = temp + b[i]
-        if (i%8 == 7):
-            output = output + chr(int(temp))
+class mutation:
+    
+    ### FUNCTIONS USED TO ASSIST MUTATION
+    # Covert input string into 
+    @staticmethod
+    def to_bytes(input: str):
+        # Returns an array of strings that represent bytes eg. ['00110011', '11001100]
+        byte_set = [bin(ord(x))[2:].zfill(8) for x in input]
+        byte_count = len(byte_set)
+        return byte_set, byte_count
+    
+    ### HERE IS WHERE ALL FUNCTIONS TO PERFORM A MUTATION SHALL BE MADE
+    # Flip a set number of consecutive bits within a byte
+    @staticmethod
+    def bitflip (input = "testing"):
+        b, number = mutation.to_bytes(input)
+        # choose number of bits to change
+        count = choices[randint(0,2)]
+        index = randint(0, number-1)
+        byte_chosen = b[index]
+        start_of_change = randint(1, bits_in_byte - count)
+        replacement = byte_chosen[:start_of_change]
+        for i in range (start_of_change, start_of_change + count):
+                if (byte_chosen[i] == '1'):
+                    replacement += '0'
+                elif (byte_chosen[i] == '0'):
+                    replacement += '1'
+        replacement += byte_chosen[start_of_change + count:]
+        b[index] = replacement
+        output = ''.join([chr(int(x, 2)) for x in b])
+        return output
+    
+    # Flip a set number of consecutive bytes
+    @staticmethod
+    def byteflip (input = "testing"):
+        b, number = mutation.to_bytes(input)
+        # choose number of bytes to change
+        count = choices[randint(0,2)]
+        output = ""
+        start_of_change = randint(0, number - 1 - count)
+        for bytes in range (start_of_change, start_of_change + count):
             temp = ""
-    return output
+            for bit in b[bytes]:
+                if (bit == '1'):
+                    temp += '0'
+                elif (bit == '0'):
+                    temp += '1'
+            b[bytes] = '0' + temp[1:] # Prevent exceed charmap
+        output = ''.join([chr(int(x, 2)) for x in b])
+        return output
+    
+    # Insert byte from different test case
+    @staticmethod
+    def insert_bytes (input = "testing", sample = "anything"):
+        # Needs other cases to copy bytes from
+        b1, number1 = mutation.to_bytes(input)
+        b2, number2 = mutation.to_bytes(sample)
+        b3 = []
+        extract = randint(0,number2 -1)
+        insertion = randint(0, number1 -1)
+        for i in range(0,insertion):
+            b3.append(b1[i])
+        b3.append(b2[extract])
+        for i in range(insertion, number1):
+            b3.append[b1[i]]
+        output = ''.join([chr(int(x, 2)) for x in b3])
+        return output
+    
+    # Change a single byte in test case
+    @staticmethod
+    def random_byte (input = "testing"): 
+        b, number = mutation.to_bytes(input)
+        # Create a byte for replacement
+        replacement = ""
+        for i in range(bits_in_byte):
+            bit = randint(0,1)
+            if (bit == 1):
+                replacement += '1'
+            elif (bit == 0):
+                replacement += '0'
+        # Choose which byte to replace
+        rbyte = randint(0, number-1)
+        b[rbyte] = replacement
+        output = ''.join([chr(int(x, 2)) for x in b])
+        return output
+    
+    # Completely remove a certain number of consecutive bytes
+    @staticmethod
+    def delete_bytes (input = "testing"):
+        b, number = mutation.to_bytes(input)
+        start_point = randint(0, number - 1)
+        limit = (number - 1) - start_point
+        count = randint(0, limit)
+        for i in range(count + 1):
+            b.pop(start_point)
+        output = ''.join([chr(int(x, 2)) for x in b])
+        return output
+    
+    # Change several bytes in a text case
+    def overwrite_bytes (input = ""):
+        # Similar to random
+        return input
 
-# Flip a set number of consecutive bytes
-def byteflip (input: str):
-    b, number = to_binary_2d(input)
-    # choose number of bytes to change
-    count = choices[randint(0,2)]
-    start_of_change = randint(0, number - 1 - count)
-    for i in range (start_of_change, start_of_change + count):
-        for j in range(bits_in_byte):
-            if (b[i][j] == '1'):
-                b[i][j] = '0'
-            elif (b[i][j] == '0'):
-                b[i][j] = '1'
-    output = "".join(chr(int("".join(x) for x in b3)))
-    return output
 
-# Change a single byte in test case
-def random_bytes (input: str):
-    b, number = to_binary_2d(input)
-    # Create a byte for replacement
-    replacement = []
-    for i in range(bits_in_byte):
-        bit = randint(0,1)
-        if (bit == 1):
-            replacement.append('1')
-        elif (bit == 0):
-            replacement.append('0')
-    # Choose which byte to replace
-    rbyte = randint(o, number-1)
-    b[rbyte] = replacement
-    output = "".join(chr(int("".join(x) for x in b3)))
-    return output
-
-# Completely remove a certain number of consecutive bytes
-def delete_bytes (input: str):
-    b, number = to_binary_2d(input)
-    start_point = randint(0, number - 1)
-    limit = (number - 1) - start_point
-    count = randint(0, limit)
-    for i in range(count + 1):
-        b.pop(start_point)
-    output = "".join(chr(int("".join(x) for x in b3)))
-    return output
-
-def insert_bytes (input: str, sample: str):
-    # Needs other cases to copy bytes from
-    b1, number1 = to_binary_2d(input)
-    b2, number2 = to_binary_2d(sample)
-    b3 = []
-    extract = randint(0,number2 -1)
-    insertion = randint(0, number1 -1)
-    for i in range(0,insertion):
-        b3.append(b1[i])
-    b3.append(b2[extract])
-    for i in range(insertion, number1):
-        b3.append[b1[i]]
-    output = "".join(chr(int("".join(x) for x in b3)))
-    return output
-
-# Change several bytes in a text case
-def overwrite_bytes (input: str):
-    # Similar to random
-    return input
+print("test")
+print(mutation.bitflip("testing"))
